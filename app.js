41bf3d0bd2aa529e8512a195f8c831b58d70b6d7
khalil <khalilonline99@gmail.com>
2022-11-01 17:38:20 +0600

    added categories and working fine

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+const categories = require('./data/categories.json')
+
+app.get('/', (req, res) => {
+  res.send('Working Fine')
+})
+
+app.get('/news-categories', (req, res)=> {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
