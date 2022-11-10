b043cffc2a86d9031c9615a823f9c5b6ee2e5e6d
khalilonline99 <khalilonline99@gmail.com>
2022-11-10 10:26:58 +0600

    changed the data

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get('/course/:id/details', (req, res) => {
   res.send(selectedCourse);
 })
 
+app.get('/checkout/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find( c => c._id == id);
+  res.send(selectedCourse);
+})
+
 app.get('/category/:id', (req, res) => {
   const id = req.params.id;
   const categoryCourse = courses.filter( c => c.category_id === id)
